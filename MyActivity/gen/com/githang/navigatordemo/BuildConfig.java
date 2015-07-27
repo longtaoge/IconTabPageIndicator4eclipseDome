1bf23a5ba790d30f7952c8629cd86aa3e512f1ad
longtaoge <61852263@qq.com>
2015-07-27 22:30:57 +0800

    IconTabPageIndicator
    IconTabPageIndicator

---
+++ MyActivity/gen/com/githang/navigatordemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.githang.navigatordemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ main/gen/com/githang/viewpagerindicator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.githang.viewpagerindicator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
